5cc2ed89b8fe61cf78e6d305078ca472e7b99f1a
Bhavya Vardhan Jain <67510492+Bhavyavardhanjain@users.noreply.github.com>
2022-10-04 12:13:02 +0530

    Added variable and method
    This file contain all method and variable operations

---
+++ python/variableandmethod.py
@@ -0,0 +1,25 @@
+#VARIABLES AND METHODS
+quote = "All is fair in love and war"
+print(quote)
+
+print(quote.upper()) #uppercase
+print(quote.lower()) #lower
+print(quote.title()) #title case
+print(len(quote)) #counts characters
+
+name = "Heath" #string
+age = 33 #int
+gpa = 3.7 #float - has a decimal
+
+print(int(age))
+print(int(30.1))
+print(int(30.9))
+
+print("My name is " + name + " and I am " + str(age) + " years old.")
+
+age +=1
+print(age)
+
+birthday = 1
+age += birthday
+print(age)
