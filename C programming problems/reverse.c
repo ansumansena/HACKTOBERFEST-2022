cfc26acbcdb25f65be218ddaeb37bcdac9ad5995
ParthThaker007 <101121538+ParthThaker007@users.noreply.github.com>
2022-10-04 18:49:32 +0530

    Update reverse.c
    Another Solution

--- C programming problems/reverse.c
+++ C programming problems/reverse.c
@@ -13,3 +13,43 @@ int main(){
         printf("%d,", arr[i]);
     }
 }
+
+
+//Solved By PartThaker007
+
+
+#include<stdio.h>
+ 
+int main() 
+{
+   int a[100], i, j, Size, Temp;
+ 
+   printf("\nPlease Enter the size : ");
+   scanf("%d",&Size);
+ 
+   //Inserting elements in to it
+   for (i = 0; i < Size; i++) 
+   {
+      scanf("%d", &a[i]);
+   }
+ 
+   j = i - 1;   // Assigning j to Last element
+   i = 0;       // Assigning i to first element
+ 
+   while (i < j) 
+   {
+      Temp = a[i];
+      a[i] = a[j];
+      a[j] = Temp;
+      i++;             
+      j--;         
+   }
+ 
+   printf("\nResult is: ");
+   for (i = 0; i < Size; i++) 
+   {
+      printf("%d \t", a[i]);
+   }
+ 
+   return 0;
+}
